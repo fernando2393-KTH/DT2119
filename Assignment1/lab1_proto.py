d04a62ce7588b32c78826940286e9dc890909991
fernando2393 <garciasanzfernando2393@gmail.com>
2020-04-02 19:45:37 +0200

    Code reformatted.

--- Assignment1/lab1_proto.py
+++ Assignment1/lab1_proto.py
@@ -2,14 +2,15 @@
 
 # Function given by the exercise ----------------------------------
 
-from Assignment1.lab1_tools import lifter, trfbank
+from lab1_tools import lifter, trfbank
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.fftpack import fft
 from scipy.fftpack.realtransforms import dct
 
-def mspec(samples, winlen = 400, winshift = 200, preempcoeff=0.97, nfft=512, samplingrate=20000):
+
+def mspec(samples, winlen=400, winshift=200, preempcoeff=0.97, nfft=512, samplingrate=20000):
     """Computes Mel Filterbank features.
 
     Args:
@@ -29,7 +30,8 @@ def mspec(samples, winlen = 400, winshift = 200, preempcoeff=0.97, nfft=512, sam
     spec = powerSpectrum(windowed, nfft)
     return logMelSpectrum(spec, samplingrate)
 
-def mfcc(samples, winlen = 400, winshift = 200, preempcoeff=0.97, nfft=512, nceps=13, samplingrate=20000, liftercoeff=22):
+
+def mfcc(samples, winlen=400, winshift=200, preempcoeff=0.97, nfft=512, nceps=13, samplingrate=20000, liftercoeff=22):
     """Computes Mel Frequency Cepstrum Coefficients.
 
     Args:
